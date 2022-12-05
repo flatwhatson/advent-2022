5aaf3d3584ab8b05e6436289418b9fd8f8482cbd
Andrew Whatson <whatson@gmail.com>
2022-12-05 16:58:42 +1000

    Day 5 part 2 solved

--- day-05.scm
+++ day-05.scm
@@ -1,7 +1,8 @@
 (use-modules (ice-9 match)
              (ice-9 peg)
              (ice-9 textual-ports)
-             (srfi srfi-1))
+             (srfi srfi-1)
+             (srfi srfi-71))
 
 (define %example "\
     [D]
@@ -59,7 +60,7 @@ move 1 from 1 to 2
      (reverse raw-stacks))
     stacks))
 
-(define (run-command command stacks)
+(define (run-command-1 command stacks)
   (define (pop-stack! ix)
     (let ((stack (vector-ref stacks ix)))
       (vector-set! stacks ix (cdr stack))
@@ -78,23 +79,44 @@ move 1 from 1 to 2
                  (iota count))
        stacks))))
 
-(define (run-commands stacks commands)
-  (fold run-command stacks commands))
+(define (run-command-2 command stacks)
+  (match command
+    (('command ('move count) ('from src) ('to dst))
+     (let ((count (string->number count))
+           (src (1- (string->number src)))
+           (dst (1- (string->number dst))))
+       (format #t "move ~a from ~a to ~a\n" count (1+ src) (1+ dst))
+       (let* ((src-stack (vector-ref stacks src))
+              (dst-stack (vector-ref stacks dst))
+              (took rest (split-at src-stack count)))
+         (vector-set! stacks src rest)
+         (vector-set! stacks dst (append took dst-stack))
+         stacks)))))
 
 (define (puzzle-0)
   (match (peg:tree (match-pattern puzzle-input %example))
-    ((('stacks . raw-stacks) ('commands . raw-commands))
-     (let* ((stacks-start (build-stacks raw-stacks))
-            (stacks-end (run-commands stacks-start raw-commands))
-            (top-crates (map first (vector->list stacks-end))))
+    ((('stacks . raw-stacks) ('commands . commands))
+     (let* ((stacks (build-stacks raw-stacks))
+            (stacks (fold run-command-1 stacks commands))
+            (top-crates (map first (vector->list stacks))))
        (apply string-append (map second top-crates))))))
 
 (define (puzzle-1)
   (define %input (call-with-input-file "data/input-05"
                    get-string-all))
   (match (peg:tree (match-pattern puzzle-input %input))
-    ((('stacks . raw-stacks) ('commands . raw-commands))
-     (let* ((stacks-start (build-stacks raw-stacks))
-            (stacks-end (run-commands stacks-start raw-commands))
-            (top-crates (map first (vector->list stacks-end))))
+    ((('stacks . raw-stacks) ('commands . commands))
+     (let* ((stacks (build-stacks raw-stacks))
+            (stacks (fold run-command-1 stacks commands))
+            (top-crates (map first (vector->list stacks))))
+       (apply string-append (map second top-crates))))))
+
+(define (puzzle-2)
+  (define %input (call-with-input-file "data/input-05"
+                   get-string-all))
+  (match (peg:tree (match-pattern puzzle-input %input))
+    ((('stacks . raw-stacks) ('commands . commands))
+     (let* ((stacks (build-stacks raw-stacks))
+            (stacks (fold run-command-2 stacks commands))
+            (top-crates (map first (vector->list stacks))))
        (apply string-append (map second top-crates))))))
