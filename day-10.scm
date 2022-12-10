525c0dba32e34d5d9fb9f9a2419f133fa5db6881
Andrew Whatson <whatson@gmail.com>
2022-12-10 20:08:45 +1000

    Day 10 minor cleanup

--- day-10.scm
+++ day-10.scm
@@ -3,159 +3,10 @@
              (srfi srfi-1)
              (srfi srfi-9))
 
-(define %example-short "\
+(define %example "\
 noop
 addx 3
 addx -5
-")
-
-(define %example-long "\
-addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop
 ")
 
 (define (parse-program str)
@@ -180,9 +31,9 @@ noop
               (result (proc (1+ cycle) regx result)))
          (loop program (+ cycle 2) (+ regx n) result))))))
 
-(define (sum-strengths start step program)
+(define (sum-strengths program)
   (fold-program (lambda (cycle regx sum)
-                  (if (= (modulo cycle step) start)
+                  (if (= (modulo cycle 40) 20)
                       (+ (* cycle regx) sum)
                       sum))
                 0 program))
@@ -200,12 +51,14 @@ noop
                 #t program))
 
 (define (puzzle-0)
-  (sum-strengths 20 40 (parse-program %example-long)))
+  (sum-strengths (parse-program
+                  (call-with-input-file "data/input-10-example"
+                    get-string-all))))
 
 (define (puzzle-1)
-  (sum-strengths 20 40 (parse-program
-                        (call-with-input-file "data/input-10"
-                          get-string-all))))
+  (sum-strengths (parse-program
+                  (call-with-input-file "data/input-10"
+                    get-string-all))))
 
 (define (puzzle-2)
   (display-pixels (parse-program
